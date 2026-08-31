480a254c89e85e3793f8c6a1710f525bbc70612a
henmority <henmority@mac.com>
2026-09-01 08:42:55 +0900

    Fix admin password clipboard helper

--- scripts/copy-admin-password.sh
+++ scripts/copy-admin-password.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -eu
 
-security find-generic-password \
-  -a admin \
-  -s 'jibunshi-admin.takatsugumihara.workers.dev' \
-  -w | tr -d '\n' | pbcopy
+osascript -e 'set the clipboard to (do shell script "/usr/bin/security find-generic-password -a admin -s jibunshi-admin.takatsugumihara.workers.dev -w")'
 
 echo '管理者パスワードをクリップボードへコピーしました。'
